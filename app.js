e4a74bb83294d4be4a5f092e5e56ba42e6c4f32e
lucidck <curtiskilgore@hotmail.com>
2020-09-23 13:48:40 -0700

    Loader, ShowError, & UserExperience

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // Listen for submit
 // Add event listener to the form button by targeting its id and using the addEventListener method
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function (e) {
+  // Hide Results so spinner can do it's thing first
+  document.getElementById("results").style.display = "none";
+
+  // Show loader when calculate is clicked/submitted
+  document.getElementById("loading").style.display = "block";
+
+  // Show results after spinner spins for 2 seconds
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 //   Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   console.log("Calculating...");
   //   Target all the stuff from the UI that you need
   //   By setting those things to variables by their id's
@@ -28,18 +37,28 @@ function calculateResults(e) {
 
   //   Check if monthly calculation equals a finite number
   if (isFinite(monthly)) {
+    // Here you insert the calculated values into the DOM
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Show Results
+    document.getElementById("results").style.display = "block";
+
+    // Hide Loader
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check yo numbers");
   }
-
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error) {
+  // Before showing error hide results and loader
+  document.getElementById("results").style.display = "none";
+
+  document.getElementById("loading").style.display = "none";
+
   // Create a div
   const errorDiv = document.createElement("div");
 
